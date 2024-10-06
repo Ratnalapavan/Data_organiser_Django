799bac997f51075e9083f07fddae18fd37f656b1
pavan <pavanratnala225@gmail.com>
2024-10-06 19:49:35 -0400

    redirection and render

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-
-def index(request):
+def index1(request):
     return HttpResponse("Hello, world. This is my first Django app!")
 
-def detail(request):
-    return HttpResponse("Text Updated to Custome.........")
-    # return render(request, 'home/detail.html')
+def index(request):
+    return render(request,"home/form.html")
+
+def submit_view(request):
+    if request.method == "POST":
+        ids = request.POST.get("your_id")
+        return redirect("success_view",ids)
+    return HttpResponse("Method is not post")
+
+def success_view(request,hi):
+    res = f"Thanks for using. Your ID is {hi}"
+    return HttpResponse(res)
+
+def detail(request,id):
+    # return HttpResponse("Text Updated to Custome.........")
+    c = {"counter":id}
+    return render(request, 'home/detail.html',c)
